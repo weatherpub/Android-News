34db6e0ef915192be33aa5915f56cfe5de0b1fc6
weatherpub <patrick@bayareamedia.net>
2024-08-12 10:34:28 -0700

    New Model

---
+++ app/src/main/java/edu/sfsu/times/model/DataModel.java
@@ -0,0 +1,61 @@
+package edu.sfsu.times.model;
+
+public class DataModel {
+    private final String id;
+    private final String name;
+    private final String author;
+    private final String title;
+    private final String description;
+    private final String url;
+    private final String urlToImage;
+    private final String publishedAt;
+    private final String content;
+
+    public DataModel(String id, String name, String author, String title, String description, String url, String urlToImage, String publishedAt, String content) {
+        this.id = id;
+        this.name = name;
+        this.author = author;
+        this.title = title;
+        this.description = description;
+        this.url = url;
+        this.urlToImage = urlToImage;
+        this.publishedAt = publishedAt;
+        this.content = content;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String getUrlToImage() {
+        return urlToImage;
+    }
+
+    public String getPublishedAt() {
+        return publishedAt;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
